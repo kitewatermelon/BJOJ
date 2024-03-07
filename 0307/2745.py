542167d732971f29a307e46ccf739434be579d23
YSPark <parkite576@gmail.com>
2024-03-07 11:06:12 +0900

    2745 has been solved

---
+++ 0307/2745.py
@@ -0,0 +1,10 @@
+N, B = input().split()
+B = int(B)
+sum = 0
+for i, n in enumerate(N):
+    operand = ord(n)-55
+    if operand < 10:
+        operand = int(n)
+    sum += operand * (B ** (len(N) -1 -i))
+
+print(sum)
